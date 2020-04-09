b0cf52d325e17fc7eabd4ee5e0ded7b03ad66792
Jacek Kałużny <duszekjk@gmail.com>
2020-04-09 10:58:32 +0200

    Initial Commit

---
+++ L-systems/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L-systems
+//
+//  Created by Jacek Kałużny on 09/04/2020.
+//  Copyright © 2020 Jacek Kałużny. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
